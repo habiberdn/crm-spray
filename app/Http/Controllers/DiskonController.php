cfc2ef2d910eeaa03082d3c684187f49b16f4868
Habib <habib@cablestat1-116.stoweaccess.com>
2025-10-09 17:55:51 +0700

    remigrate diskon schema, added validator diskon controller

--- app/Http/Controllers/DiskonController.php
+++ app/Http/Controllers/DiskonController.php
@@ -43,26 +43,18 @@ class DiskonController extends Controller
             'type' => ['required', 'string', 'max:255'],
             'value' => ['required', 'image', 'mimes:png,jpg,jpeg'],
             'start_date' => ['required', 'string', 'max:65535'],
-            'start_date' => ['required', 'string', 'max:65535'],
-
+            'end__datetime' => ['required', 'string', 'max:65535'],
+            'status' => ['required', 'string', 'max:65535'],
         ]);
         
         DB::beginTransaction();
 
         try{
-            if($request->hasFile('cover')){
-                $coverPath = $request->file('cover')->store('product_covers', 'public');
-                $validated['cover'] = $coverPath;
-            }
-            if($request->hasFile('path_file')){
-                $path_filePath = $request->file('path_file')->store('product_files', 'public');
-                $validated['path_file'] = $path_filePath;
-            }
-            $validated['creator_id'] = Auth::id();
-            $newProduct = Product::create($validated);
+           
+            Diskon::create($validated);
             DB::commit();
 
-            return redirect()->route('admin.products.index')->with('success', 'Product created successfuly!');
+            return redirect()->route('admin.diskon.index')->with('success', 'Diskon created successfuly!');
         }
         catch(\Exception $e){
             DB::rollBack();
